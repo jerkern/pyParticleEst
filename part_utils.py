6cc6a91ee69c441b38174d5a82435f56781015f4
Jerker Nordh <ajn@ajn.se>
2013-04-12 17:00:17 +0200

    Implemented ABC interface instead of raising NotImplemented exceptions.

--- part_utils.py
+++ part_utils.py
@@ -1,69 +1,87 @@
 """ Collection of functions and classes used for Particle Filtering/Smoothing """
-
+import abc
 
 class ParticleFilteringBase(object):
     """ Base class for particles to be used with particle filtering """
+    __metaclass__ = abc.ABCMeta
+     
+    @abc.abstractmethod
     def sample_input_noise(self, u):
         """ Return a noise perturbed input vector u """
-        raise NotImplementedError( "Should have implemented this" )
+        return
     
+    @abc.abstractmethod
     def update(self, data):
-        raise NotImplementedError( "Should have implemented this" )
-        
+        """ Update estimate using 'data' as input """
+        return
+    
+    @abc.abstractmethod    
     def measure(self, y):
-        raise NotImplementedError( "Should have implemented this" )
+        """ Return the log-pdf value of the measurement """
+        return
 
 
 
 class ParticleSmoothingBase(ParticleFilteringBase):
     """ Base class for particles to be used with particle smoothing """
-    def next_pdf(self, next_cpart, u):
-        """ Return the probability density value for the possible future state 'next' given input u """
-        raise NotImplementedError( "Should have implemented this" )
     
+    @abc.abstractmethod
+    def next_pdf(self, next_cpart, u):
+        """ Return the log-pdf value for the possible future state 'next' given input u """
+        return
+    
+    @abc.abstractmethod
     def sample_smooth(self, filt_traj, ind, next_cpart):
         """ Return a collapsed particle with the rao-blackwellized states sampled """
-        raise NotImplementedError( "Should have implemented this" )
+        return
     
+    @abc.abstractmethod
     def collapse(self):
         """ Return a sample of the particle where the rao-blackwellized states
         are drawn from the MVN that results from CLGSS structure """
-        
-        raise NotImplementedError( "Should have implemented this" )
+        return
     
 class ParticleSmoothingBaseRB(ParticleSmoothingBase):
     
+    @abc.abstractmethod
     def clin_update(self, u):
         """ Kalman update of the linear states conditioned on the non-linear trajectory estimate """
-        raise NotImplementedError( "Should have implemented this" )
+        return
     
+    @abc.abstractmethod
     def clin_measure(self, y):
         """ Kalman measuement of the linear states conditioned on the non-linear trajectory estimate """
-        raise NotImplementedError( "Should have implemented this" )
+        return
 
+    @abc.abstractmethod
     def clin_smooth(self, z_next, u):
         """ Kalman smoothing of the linear states conditioned on the next particles linear states """ 
-        raise NotImplementedError( "Should have implemented this" )
+        return
 
+    @abc.abstractmethod
     def set_nonlin_state(self, eta):
         """ Set the non-linear state estimates """
-        raise NotImplementedError( "Should have implemented this" )
+        return
     
+    @abc.abstractmethod
     def get_nonlin_state(self):
         """ Return the non-linear state estimates """
-        raise NotImplementedError( "Should have implemented this" )
+        return
     
+    @abc.abstractmethod
     def set_lin_est(self, lest):
         """ Set the estimate of the rao-blackwellized states """
-        raise NotImplementedError( "Should have implemented this" )
+        return
  
+    @abc.abstractmethod
     def get_lin_est(self):
         """ Return the estimate of the rao-blackwellized states """
-        raise NotImplementedError( "Should have implemented this" )
+        return
  
+    @abc.abstractmethod
     def linear_input(self, u):
         """ Extract the part of u affect the conditionally rao-blackwellized states """
-        raise NotImplementedError( "Should have implemented this" )    
+        return
     
     
 
