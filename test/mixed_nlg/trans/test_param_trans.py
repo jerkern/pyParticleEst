2b90e4b9458a2af9485ebcb9527d5819f3d21502
Jerker Nordh <ajn@ajn.se>
2013-09-25 13:58:15 +0200

    Changed plotting in test_param_trans.py

--- test/mixed_nlg/trans/test_param_trans.py
+++ test/mixed_nlg/trans/test_param_trans.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python
 
-import PF
-import PS
 import numpy
 import param_est
 import copy
@@ -26,7 +24,7 @@ class ParticleParamTransEst(param_est.ParamEstimation):
 if __name__ == '__main__':
     
     num = 50
-    nums=10
+    nums = 10
     
     theta_true = 0.1
     theta_guess = 0.3   
@@ -37,7 +35,7 @@ if __name__ == '__main__':
 
     # How many steps forward in time should our simulation run
     steps = 200
-    sims = 20
+    sims = 130
 
     # Create arrays for storing some values for later plotting    
     vals = numpy.zeros((2, num+1, steps+1))
@@ -49,6 +47,7 @@ if __name__ == '__main__':
     
     plt.ion()
     fig1 = plt.figure()
+    fig2 = plt.figure()
     
     for k in range(sims):
         # Create reference
@@ -77,19 +76,15 @@ if __name__ == '__main__':
         
         print "estimation start"
         
+        plt.figure(fig1.number)
         plt.clf()
         x = numpy.asarray(range(steps+1))
         plt.plot(x[1:],numpy.asarray(y_noise)[:,0],'b.')
         plt.plot(range(steps+1),vals[0,num,:],'go')
         plt.plot(range(steps+1),vals[1,num,:],'ro')
-            
-        
-        
-        fig1.show()
         plt.title("Param = %s" % theta_true)
-        plt.show()
-        plt.draw()
-
+        fig1.show()
+            
         # Create an array for our particles 
         ParamEstimator = ParticleParamTransEst(u=None, y=y_noise)
         ParamEstimator.set_params(numpy.array((theta_guess,)).reshape((-1,1)))
@@ -115,17 +110,25 @@ if __name__ == '__main__':
             plt.plot(range(steps+1),svals[0,j,:],'g-')
             plt.plot(range(steps+1),svals[1,j,:],'r-')
 
-        fig1.show()
-        plt.title("Param = %s" % theta_true)
-        plt.show()
-        plt.draw()
+
+        
+
+        
         
         print "maximization start"
         
         estimate[0,k] = param
-    
+        
+        plt.figure(fig2.number)
+        plt.clf()
+        plt.hist(estimate[0,:(k+1)].T)
+        fig2.show()
+        plt.show()
+        plt.draw()
 
-    fig2 = plt.figure()
+        
+
+    
     plt.hist(estimate.T)
     plt.ioff()
     plt.show()
