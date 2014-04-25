d035734604add2536ddf1f65862fe1fcc431bed0
Jerker Nordh <ajn@ajn.se>
2014-04-25 11:45:48 +0200

    Cleaned up lsB testcase.

--- pyparticleest/test/filtering/test_lsb.py
+++ pyparticleest/test/filtering/test_lsb.py
@@ -1,12 +1,16 @@
 '''
-Example B from 
-Rao-Blackwellized particle smoothers for mixed linear/nonlinear state-space models
-Fredrik Lindsten, Thomas B. Schon
+Created on Nov 11, 2013
+
+@author: ajn
 '''
 
 import numpy
 import math
+import matplotlib.pyplot as plt
 from pyparticleest.models.mlnlg import MixedNLGaussianInitialGaussian
+import pyparticleest.param_est as param_est
+import scipy.io
+import sys
 
 C_theta = numpy.array([[ 0.0, 0.04, 0.044, 0.08],])
 def calc_Ae_fe(eta, t):
@@ -104,7 +108,7 @@ class ParticleLSB(MixedNLGaussianInitialGaussian):
         h = h[:,numpy.newaxis,numpy.newaxis]
         
         return (numpy.asarray(y).reshape((-1,1)), None, h, None)
-    
+
 #class ParticleLSB_JN(ParticleLSB):
 #    """ Model 60 & 61 from Lindsten & Schon (2011) """
 #    def __init__(self):
@@ -157,3 +161,115 @@ class ParticleLSB(MixedNLGaussianInitialGaussian):
 ##        self.t = t_old
 #        
 #        return logpy
+
+if __name__ == '__main__':
+    
+    num = 300
+    nums = 10
+        
+    # How many steps forward in time should our simulation run
+    steps = 100
+    
+    sims = 1000
+    
+    if (len(sys.argv) > 1):
+        if (sys.argv[1] == 'nogui'):
+        
+            sqr_err_eta = numpy.zeros((sims, steps+1))
+            sqr_err_theta = numpy.zeros((sims, steps+1))
+
+            C_theta = numpy.array([[ 0.0, 0.04, 0.044, 0.08],])
+            for k in range(sims):
+                # Create reference
+                numpy.random.seed(k)
+                (y, e, z) = generate_dataset(steps)
+        
+                model = ParticleLSB()
+                # Create an array for our particles 
+                ParamEstimator = param_est.ParamEstimation(model=model, u=None, y=y)
+                ParamEstimator.simulate(num, nums, res=0.67, filter='PF', smoother='full')
+        
+                svals = numpy.zeros((2, nums, steps+1))
+                
+                for i in range(steps+1):
+                    (xil, zl, Pl) = model.get_states(ParamEstimator.straj.traj[i])
+                    svals[0,:,i] = numpy.vstack(xil).ravel()
+                    svals[1,:,i] = 25.0+C_theta.dot(numpy.hstack(zl)).ravel()
+                
+                # Use average of trajectories
+                svals_mean = numpy.mean(svals,1)
+        
+                theta = 25.0+C_theta.dot(z.reshape((4,-1)))
+                sqr_err_eta[k,:] = (svals_mean[0,:] - e[0,:])**2
+                sqr_err_theta[k,:] = (svals_mean[1,:] - theta)**2
+        
+                rmse_eta = numpy.sqrt(numpy.mean(sqr_err_eta[k,:]))
+                rmse_theta = numpy.sqrt(numpy.mean(sqr_err_theta[k,:]))
+                print "%d %f %f" % (k, numpy.mean(rmse_eta), numpy.mean(rmse_theta))
+            
+    else:
+    
+
+    
+        # Create arrays for storing some values for later plotting    
+        vals = numpy.zeros((2, num+1, steps+1))
+    
+        plt.ion()
+
+        # Create reference
+        #numpy.random.seed(1)
+        #numpy.random.seed(86)
+        (y, e, z) = generate_dataset(steps)
+        # Store values for last time-step aswell    
+    
+        
+        x = numpy.asarray(range(steps+1))
+        model = ParticleLSB()
+        # Create an array for our particles 
+        ParamEstimator = param_est.ParamEstimation(model=model, u=None, y=y)
+        ParamEstimator.simulate(num, nums, res=0.67, filter='PF', smoother='full')
+
+        
+        svals = numpy.zeros((2, nums, steps+1))
+        vals = numpy.zeros((2, num, steps+1))
+ 
+        for i in range(steps+1):
+            (xil, zl, Pl) = model.get_states(ParamEstimator.straj.traj[i])
+            svals[0,:,i] = numpy.vstack(xil).ravel()
+            svals[1,:,i] = 25.0+C_theta.dot(numpy.hstack(zl)).ravel()
+            (xil, zl, Pl) = model.get_states(ParamEstimator.pt.traj[i].pa.part)
+            vals[0,:,i]=numpy.vstack(xil).ravel()
+            vals[1,:,i]=25.0+C_theta.dot(numpy.hstack(zl)).ravel()
+                
+        svals_mean = numpy.mean(svals,1)
+        plt.figure()
+
+        for j in range(num):   
+            plt.plot(range(steps+1),vals[0,j,:],'.', markersize=3.0, color='#BBBBBB')
+        plt.plot(x, e.T,'k-',markersize=1.0)
+        for j in range(nums):
+            plt.plot(range(steps+1),svals[0,j,:],'--', markersize=2.0, color='#999999', dashes=(7,50))
+        #plt.plot(range(steps+1),svals_mean[0,:],'--', markersize=1.0, color='1.0')
+
+        #plt.savefig('rbps_fail_xi.eps', bbox_inches='tight')
+        plt.show()
+        
+        plt.figure()
+        for j in range(num):   
+            plt.plot(range(steps+1),vals[1,j,:],'.', markersize=3.0, color='#BBBBBB')
+        plt.plot(x, (25.0+C_theta.dot(z)).ravel(),'k-',markersize=1.0)
+        for j in range(nums):
+            plt.plot(range(steps+1),svals[1,j,:],'--', markersize=2.0, color='#999999', dashes=(10,25))
+        #plt.plot(range(steps+1),svals_mean[1,:],'--', markersize=1.0, color='1.0')
+        #plt.savefig('rbps_fail_theta.eps', bbox_inches='tight')
+        plt.show()
+
+        plt.draw()
+        # Export data for plotting in matlab
+        scipy.io.savemat('test_lsb.mat', {'svals_mean': svals_mean, 'vals': vals, 
+                                          'svals': svals, 'y': y, 'z': z, 'e':e})
+        
+        plt.ioff()
+        plt.show()
+        plt.draw()
+    print "exit"
