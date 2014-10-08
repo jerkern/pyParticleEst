7d3c8d87a8d1cc835f6a4fbd1bebb5c3b96d8cfb
Jerker Nordh <ajn@ajn.se>
2014-10-08 14:18:58 +0200

    Build dependencies

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
 from distutils.core import setup
 setup(name='pyParticleEst',
       version='1.0',
-      packages=['pyparticleest', 'pyparticleest/models', 'pyparticleest/paramest'],
+      packages=['pyparticleest', 'pyparticleest/models', 'pyparticleest/paramest', 'pyparticleest/utils'],
       url='http://www.control.lth.se/Staff/JerkerNordh/pyparticleest.html',
       author='Jerker Nordh',
       author_email='jerker.nordh@control.lth.se',
       description='Framework for particle based estimation methods, such as particle filtering and smoothing',
       license='LGPL',
+      install_requires = [
+	'numpy',
+	'scipy',
+	'cython',
+	]
       )
